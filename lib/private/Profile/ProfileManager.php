1361e885414799a3d5e779833adee3f453e55eaf
Thomas Citharel <tcit@tcit.fr>
2023-03-10 11:56:27 +0100

    fix(profile): Set profile config cache directly after writing it to DB
    In ProfileManager::getProfileParams we make a lot of indirect calls to ProfileManager::getProfileConfig. The first time we get the call we get into the catch and insert data, and the next time we miss the cache and do the select. There's a possibility of read-after-write issue here (the database r/o-mirror doesn't have yet the inserted config), which leads to conflicts when it tries to reinsert the config. To avoid that, let's put the config value in the configcache directly when it's written.
    
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- lib/private/Profile/ProfileManager.php
+++ lib/private/Profile/ProfileManager.php
@@ -385,6 +385,7 @@ class ProfileManager {
 				$this->filterNotStoredProfileConfig($config->getConfigArray()),
 			));
 			$this->configMapper->update($config);
+			$this->configCache[$targetUser->getUID()] = $config;
 			$configArray = $config->getConfigArray();
 		} catch (DoesNotExistException $e) {
 			// Create a new default config if it does not exist
@@ -392,6 +393,7 @@ class ProfileManager {
 			$config->setUserId($targetUser->getUID());
 			$config->setConfigArray($defaultProfileConfig);
 			$this->configMapper->insert($config);
+			$this->configCache[$targetUser->getUID()] = $config;
 			$configArray = $config->getConfigArray();
 		}
 
