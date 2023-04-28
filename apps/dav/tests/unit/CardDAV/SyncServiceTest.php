e866a954a51ac814976b959a1746379705ae2894
Christoph Wurst <christoph@winzerhof-wurst.at>
2023-04-28 12:32:18 +0200

    fix(dav): Run system address book create-if-not-exists in transaction
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- apps/dav/tests/unit/CardDAV/SyncServiceTest.php
+++ apps/dav/tests/unit/CardDAV/SyncServiceTest.php
@@ -30,6 +30,7 @@ namespace OCA\DAV\Tests\unit\CardDAV;
 use OCA\DAV\CardDAV\CardDavBackend;
 use OCA\DAV\CardDAV\Converter;
 use OCA\DAV\CardDAV\SyncService;
+use OCP\IDBConnection;
 use OCP\IUser;
 use OCP\IUserManager;
 use Psr\Log\LoggerInterface;
@@ -151,7 +152,7 @@ class SyncServiceTest extends TestCase {
 	 * @param int $createCount
 	 * @param int $updateCount
 	 * @param int $deleteCount
-	 * @return \PHPUnit\Framework\MockObject\MockObject
+	 * @return \PHPUnit\Framework\MockObject\MockObject|CardDavBackend
 	 */
 	private function getBackendMock($createCount, $updateCount, $deleteCount) {
 		$backend = $this->getMockBuilder(CardDavBackend::class)
@@ -170,12 +171,13 @@ class SyncServiceTest extends TestCase {
 	 */
 	private function getSyncServiceMock($backend, $response) {
 		$userManager = $this->getMockBuilder(IUserManager::class)->disableOriginalConstructor()->getMock();
+		$dbConnection = $this->createMock(IDBConnection::class);
 		$logger = $this->getMockBuilder(LoggerInterface::class)->disableOriginalConstructor()->getMock();
 		$converter = $this->createMock(Converter::class);
 		/** @var SyncService | \PHPUnit\Framework\MockObject\MockObject $ss */
 		$ss = $this->getMockBuilder(SyncService::class)
 			->setMethods(['ensureSystemAddressBookExists', 'requestSyncReport', 'download', 'getCertPath'])
-			->setConstructorArgs([$backend, $userManager, $logger, $converter])
+			->setConstructorArgs([$backend, $userManager, $dbConnection, $logger, $converter])
 			->getMock();
 		$ss->method('requestSyncReport')->withAnyParameters()->willReturn(['response' => $response, 'token' => 'sync-token-1']);
 		$ss->method('ensureSystemAddressBookExists')->willReturn(['id' => 1]);
